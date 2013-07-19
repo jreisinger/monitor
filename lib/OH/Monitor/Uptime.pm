7967f70797f07de57ac2e1994f7389a9e678e0ca
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-07-19 14:20:08 +0200

    Check availability also by connecting to some tcp ports

--- lib/OH/Monitor/Uptime.pm
+++ lib/OH/Monitor/Uptime.pm
@@ -4,21 +4,35 @@ use Exporter qw(import);
 our @EXPORT_OK = qw (is_up);
 
 use Net::Ping;
+use IO::Socket;
 
 ###################
 sub is_up {
 ###################
     my $host = shift;
-    my @down;
-    my $p = Net::Ping->new();
-    push @down, $host unless $p->ping($host);
-    $p->close();
+    my $up;
 
-    if (@down) {
-        return "down";
-    } else {
+    # try connection to echo port
+    my $p = Net::Ping->new();
+    if ( $p->ping($host) ) {
+        $p->close();
         return 'ok';
     }
+
+    # try connections to other ports
+    for my $port (qw(22 80 443)) {
+        my $sock = new IO::Socket::INET(
+            PeerAddr => $host,
+            PeerPort => $port,
+            Proto    => 'tcp',
+        );
+        if ($sock) {
+            close($sock);
+            return 'ok';
+        }
+    }
+
+    return "down";
 }
 
 1;
