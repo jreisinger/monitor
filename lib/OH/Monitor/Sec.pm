9f5e7affed2332beb2d810af82abb205d0cfe1c0
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-10-07 18:44:44 +0200

    add possibility to define known origins

--- lib/OH/Monitor/Sec.pm
+++ lib/OH/Monitor/Sec.pm
@@ -5,19 +5,35 @@ our @EXPORT_OK = qw (last_from);
 
 use Net::SSH qw(ssh_cmd);
 use List::MoreUtils qw(uniq);
+use File::Basename qw(dirname);
+use YAML qw(Load);
 
+# load configuration from config file
+sub _load_config {
+
+    my $conf_file =
+      dirname( $INC{'OH/Monitor/Sec.pm'} ) . '/../../../t/' . '.conf.yml';
+    die "'$conf_file' problem: $!" unless -e $conf_file;
+    my $content = eval { local ( @ARGV, $/ ) = ($conf_file); <>; };
+    my $config = Load($content);
+
+    return $config;
+}
+
+# Check whether user has logged in from different hosts using the last system command.
 sub last_from {
     my $host = shift;
     my $cmd  = 'last -aw';
 
+    # run $cmd system command locally or remotely
     my $cmd_lines = ( $host eq 'localhost' )
       ? `$cmd`
 
       # ssh_cmd - STDOUT returned as single string STDERR throws fatal error
       : eval { ssh_cmd( "root\@$host", $cmd ) };
 
-    my %origins;
-
+    # collect login origins for different users
+    my %origins;    # user => [ qw(host1 host2) ]
     for ( split '\n', $cmd_lines ) {
 
         # skip uninteresting lines
@@ -33,11 +49,20 @@ sub last_from {
         # remove duplicated origins
         @{ $origins{$user} } = uniq @{ $origins{$user} };
 
+        # remove known origins (set in t/.conf.yml)
+        my %temp       = map { $_, 1 } @{ $origins{$user} };
+        my $config     = _load_config();
+        my @ok_origins = @{ $config->{'sec-checks'}->{'ok-origins'} };
+        delete $temp{$_} for @ok_origins;
+        @{ $origins{$user} } = keys %temp;
+
+        # remove users who have logged in from 1 origin only
         if ( @{ $origins{$user} } == 1 ) {
             delete $origins{$user};
         }
     }
 
+    # return check result
     if ( keys %origins ) {
         return \%origins;    # ref to HoA
     } else {
